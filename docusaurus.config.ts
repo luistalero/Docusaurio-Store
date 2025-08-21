f6ed248a5818759c101dcdf86e34c5da272d77d0
Luis Alberto Talero Martinez <luis.talero@wiedii.co>
2025-08-21 14:25:33 -0500

    change minimun

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -40,6 +40,12 @@ const config: Config = {
       },
     },
     {
+      tagName: "title",
+      attributes: {
+        innerHTML: "Wiedii Documentation",
+      },
+    },
+    {
       tagName: "link",
       attributes: {
         rel: "icon",
